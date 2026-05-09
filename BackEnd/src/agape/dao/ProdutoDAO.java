b7cfe00970519b9d823e01e485b527323b17deb4
chriscell1956 <christianorgdo@gmail.com>
2026-05-09 17:36:42 -0300

    fix: Corrigindo nome da coluna de estoque para qtdAtual

--- BackEnd/src/agape/dao/ProdutoDAO.java
+++ BackEnd/src/agape/dao/ProdutoDAO.java
@@ -10,7 +10,7 @@ public class ProdutoDAO {
 
     // Recebe a conexão 'conn' por parâmetro conforme desenho do Humberto
     public void atualizarEstoque(Connection conn, int idProd, int quantidade) throws Exception {
-        String sql = "UPDATE Produto SET qtdeAtual = qtdeAtual + ? WHERE idProd = ?";
+        String sql = "UPDATE Produto SET qtdAtual = qtdAtual + ? WHERE idProd = ?";
         
         try (PreparedStatement stmt = conn.prepareStatement(sql)) {
             stmt.setInt(1, quantidade);
