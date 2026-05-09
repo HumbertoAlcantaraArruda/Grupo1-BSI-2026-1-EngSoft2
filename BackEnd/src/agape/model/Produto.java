6fdf424129b58355992fa28eb9a5543dafa82dfc
chriscell1956 <christianorgdo@gmail.com>
2026-05-09 18:21:49 -0300

    feat: Adicionando metodo toJson nos models para visualizacao no navegador

--- BackEnd/src/agape/model/Produto.java
+++ BackEnd/src/agape/model/Produto.java
@@ -1,51 +1,32 @@
 package agape.model;
 
-import java.time.LocalDateTime;
-
 public class Produto {
     private int idProd;
     private int idCatProd;
     private String nome;
     private float valorUni;
-    private int qtdeAtual;
+    private int qtdAtual;
 
-    public int getIdProd() {
-        return idProd;
-    }
+    public Produto() {}
 
-    public void setIdProd(int idProd) {
-        this.idProd = idProd;
-    }
+    // Getters e Setters
+    public int getIdProd() { return idProd; }
+    public void setIdProd(int idProd) { this.idProd = idProd; }
+    public int getIdCatProd() { return idCatProd; }
+    public void setIdCatProd(int idCatProd) { this.idCatProd = idCatProd; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public float getValorUni() { return valorUni; }
+    public void setValorUni(float valorUni) { this.valorUni = valorUni; }
+    public int getQtdAtual() { return qtdAtual; }
+    public void setQtdAtual(int qtdAtual) { this.qtdAtual = qtdAtual; }
 
-    public int getIdCatProd() {
-        return idCatProd;
-    }
-
-    public void setIdCatProd(int idCatProd) {
-        this.idCatProd = idCatProd;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public float getValorUni() {
-        return valorUni;
-    }
-
-    public void setValorUni(float valorUni) {
-        this.valorUni = valorUni;
-    }
-
-    public int getQtdeAtual() {
-        return qtdeAtual;
-    }
-
-    public void setQtdeAtual(int qtdeAtual) {
-        this.qtdeAtual = qtdeAtual;
+    public String toJson() {
+        return "{" +
+            "\"idProd\":" + idProd + "," +
+            "\"nome\":\"" + nome + "\"," +
+            "\"valorUni\":" + valorUni + "," +
+            "\"qtdAtual\":" + qtdAtual +
+            "}";
     }
 }
--- BackEnd/src/agape/model/Usuario.java
+++ BackEnd/src/agape/model/Usuario.java
@@ -1,90 +1,36 @@
 package agape.model;
 
-import java.time.LocalDateTime;
-
 public class Usuario {
-
     private int idUsuario;
     private String nome;
-    private String cpf;
-    private String email;
+    private String login;
     private String senha;
     private int status;
-    private String nivel;
-    private LocalDateTime dataAtivacao;
-    private LocalDateTime dataDesativacao;
+    private String perfil;
 
-    // GETTERS E SETTERS
+    public Usuario() {}
 
-    public int getIdUsuario() {
-        return idUsuario;
-    }
+    // Getters e Setters
+    public int getIdUsuario() { return idUsuario; }
+    public void setIdUsuario(int idUsuario) { this.idUsuario = idUsuario; }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    public String getLogin() { return login; }
+    public void setLogin(String login) { this.login = login; }
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+    public int getStatus() { return status; }
+    public void setStatus(int status) { this.status = status; }
+    public String getPerfil() { return perfil; }
+    public void setPerfil(String perfil) { this.perfil = perfil; }
 
-    public void setIdUsuario(int idUsuario) {
-        this.idUsuario = idUsuario;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getCpf() {
-        return cpf;
-    }
-
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getSenha() {
-        return senha;
-    }
-
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
-    public String getNivel() {
-        return nivel;
-    }
-
-    public void setNivel(String nivel) {
-        this.nivel = nivel;
-    }
-
-    public LocalDateTime getDataAtivacao() {
-        return dataAtivacao;
-    }
-
-    public void setDataAtivacao(LocalDateTime dataAtivacao) {
-        this.dataAtivacao = dataAtivacao;
-    }
-
-    public LocalDateTime getDataDesativacao() {
-        return dataDesativacao;
-    }
-
-    public void setDataDesativacao(LocalDateTime dataDesativacao) {
-        this.dataDesativacao = dataDesativacao;
+    public String toJson() {
+        return "{" +
+            "\"idUsuario\":" + idUsuario + "," +
+            "\"nome\":\"" + nome + "\"," +
+            "\"login\":\"" + login + "\"," +
+            "\"perfil\":\"" + perfil + "\"," +
+            "\"status\":" + status +
+            "}";
     }
 }
